6bdaef84506b1f62cd9cdcc1b3aea2073befd019
Daniel John Causer <DanielCauser@users.noreply.github.com>
2018-01-15 16:42:51 -0500

    Work on recycle view for android

---
+++ XamarinHorizontalList/XamarinHorizontalList.Android/AndroidHorizontalViewRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamarinHorizontalList.Droid;
+using XamarinHorizontalList.Renderers;
+
+[assembly: ExportRenderer(typeof(HorizontalViewRenderer), typeof(AndroidHorizontalViewRenderer))]
+namespace XamarinHorizontalList.Droid
+{
+	public class AndroidHorizontalViewRenderer : ViewRenderer<HorizontalViewRenderer, RecyclerView>
+	{
+	}
+}
